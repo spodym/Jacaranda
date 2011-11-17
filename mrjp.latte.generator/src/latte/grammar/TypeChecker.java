3a462094054351f9880098e2fd2388e825dce450
Przemysław Spodymek <madvril@gmail.com>
2011-11-17 22:14:15 +0100

    Type checker draft.

---
+++ mrjp.latte.generator/src/latte/grammar/TypeChecker.java
@@ -0,0 +1,19 @@
+package latte.grammar;
+
+import latte.grammar.latteParser.program_return;
+
+import org.antlr.runtime.ANTLRStringStream;
+import org.antlr.runtime.CharStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.TokenStream;
+import org.antlr.runtime.tree.CommonTreeNodeStream;
+
+
+public class TypeChecker {
+
+	public void checkTypes(lattetree tree) throws TypesMismatchException {
+		
+	}
+	
+}
---
+++ mrjp.latte.generator/src/latte/grammar/TypesMismatchException.java
@@ -0,0 +1,9 @@
+package latte.grammar;
+
+public class TypesMismatchException extends Exception {
+	private static final long serialVersionUID = -5587373151372831328L;
+
+	public TypesMismatchException(String message) {
+		super(message);
+	}
+}
